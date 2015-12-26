5992538ca3258bb32aa22f5dca3685e2deff981e
JAKE <jakeLu@JakeLus-MBP.lan>
2015-12-26 01:40:21 -0800

    start

---
+++ Cuisine-God/Cuisine-God/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cuisine-God
+//
+//  Created by JAKE on 12/26/15.
+//  Copyright © 2015 Jake Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
